adc7c52bb2bca780c3e28a008c0e7a8308712845
jgrantham <insignificant786@gmail.com>
2019-08-13 12:15:42 -0500

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -10,6 +10,7 @@
 wireguard_install(){
 
         echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d/unstable.list
+        apt-key adv --keyserver   keyserver.ubuntu.com --recv-keys 8B48AD6246925553
         printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
         apt update
         apt install wireguard resolvconf qrencode raspberrypi-kernel-headers dirmngr -y
